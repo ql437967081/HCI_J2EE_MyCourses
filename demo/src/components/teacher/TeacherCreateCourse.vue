beb0c52b6678bffaec994eaf4e261b0cb7e695ed
161250038 <161250038@smail.nju.edu.cn>
2019-06-23 21:19:45 +0800

    upload teacher's vue

---
+++ demo/src/components/teacher/TeacherCreateCourse.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "TeacherCreateCourse"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ demo/src/components/teacher/TeacherPublishCourse.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "TeacherPublishCourse"
+    }
+</script>
+
+<style scoped>
+
+</style>
